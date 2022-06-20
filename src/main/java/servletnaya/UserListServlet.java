07875b3e618bfdd4b0d4a9920155523f16268a42
Димас <gigakot@yandex.ru>
2022-06-21 00:38:51 +0300

    add new user

--- src/main/java/servletnaya/UserListServlet.java
+++ src/main/java/servletnaya/UserListServlet.java
@@ -21,6 +21,7 @@ public class UserListServlet extends HttpServlet {
             add(new User("Gnyava","Buba"));
             add(new User("Shmiga","Boba"));
             add(new User("Motya","Kotya"));
+            add(new User("chupa","chups"));
         }};
 
         req.setAttribute("users", users);
