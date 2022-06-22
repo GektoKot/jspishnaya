78c0d5d374908807ff9ca1df64a59258982b3087
Димас <gigakot@yandex.ru>
2022-06-22 13:56:36 +0300

    delete dublicates

--- src/main/java/servletnaya/UserListServlet.java
+++ src/main/java/servletnaya/UserListServlet.java
@@ -17,17 +17,8 @@ public class UserListServlet extends HttpServlet {
             add(new User("Vasya","Pupkin"));
             add(new User("Petya","Petushok"));
             add(new User("Tetya","Dyadya"));
-            add(new User("Tetya","Dyadya"));
-            add(new User("Tetya","Dyadya"));
-            add(new User("Tetya","Dyadya"));
-            add(new User("Tetya","Dyadya"));
-            add(new User("Tetya","Dyadya"));
             add(new User("Dril","Biba"));
             add(new User("Gnyava","Buba"));
-            add(new User("Gnyava","Buba"));
-            add(new User("Gnyava","Buba"));
-            add(new User("Gnyava","Buba"));
-            add(new User("Shmiga","Boba"));
             add(new User("Motya","Kotya"));
             add(new User("chupa","chups"));
         }};
